1c20273c9991cd6a09e5fb490f5bdbaceece51bf
mmszhezhi <mmszhezhi@gmail.com>
2021-12-29 12:05:05 +0800

    init

--- no2.py
+++ no2.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 def readdfs():
-    dfs = pd.read_excel("energy2.xlsx", index_col=0, sheet_name=None)
+    dfs = pd.read_excel("no2.xlsx", sheet_name=None)
     return dfs
 
 
@@ -38,11 +38,36 @@ def process(df):
     f2 = factor[df.columns].loc["transfactor", :]
     return df*f1*f2
 
-with pd.ExcelWriter("pm_by_industry.xlsx") as writer:
+
+def process2(df):
+    if df["二氧化氮"].dtype != float:
+        df["二氧化氮"] = df["二氧化氮"].astype(str)
+        df["二氧化氮"].str.isdigit()
+        df = df[df["二氧化氮"].str.isdigit()]
+        df["二氧化氮"] = df["二氧化氮"].astype(float)
+    no2_per_y = df.groupby(by=["行政区名称"])["二氧化氮"].mean()
+    return no2_per_y
+
+
+def process3(df):
+    if df["二氧化氮"].dtype != float:
+        df["二氧化氮"] = df["二氧化氮"].astype(str)
+        df["二氧化氮"].str.isdigit()
+        df = df[df["二氧化氮"].str.isdigit()]
+        df["二氧化氮"] = df["二氧化氮"].astype(float)
+    df["监测时间"] = df["监测时间"].apply(lambda x: x[:-3])
+    no2_per_m = df.groupby(by=["行政区名称", "监测时间"])["二氧化氮"].mean()
+    # no2_per_y = df.groupby(by=["行政区名称"])["二氧化氮"].mean()
+    return no2_per_m
+
+
+
+with pd.ExcelWriter("no2_per_m.xlsx") as writer:
     for s in sheets:
         df = dfs.get(s)
-        result = process(df)
+        result = process3(df)
         result.to_excel(writer,sheet_name=s)
+        print(s)
 
 
 
